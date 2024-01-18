adaf49e3ce8650d3d39ee8c37f57146272d823af
Harrenn <71885799+Harrenn@users.noreply.github.com>
2024-01-18 18:01:15 +0800

    Add files via upload

---
+++ scrape.py
@@ -0,0 +1,63 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup
+from urllib.parse import urlparse
+import pandas as pd
+from multiprocessing import Pool
+
+def scrape_links(url):
+    driver = webdriver.Chrome()  # Use the browser driver you installed
+    driver.get(url)
+    
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+    driver.quit()
+    
+    instagram_link = ''
+    email_link = ''
+    username = ''  # Initialize username with an empty string
+    
+    for link in soup.find_all('a'):
+        href = link.get('href')
+        if href and 'instagram.com' in href and not instagram_link:
+            # If it's a relative URL, add the base URL
+            if href.startswith('/'):
+                href = url + href
+            # Parse the URL and rebuild it without query parameters
+            parsed_url = urlparse(href)
+            href = parsed_url.scheme + "://" + parsed_url.netloc + parsed_url.path
+            # Prioritize links without 'www'
+            if 'www.' not in href:
+                instagram_link = href
+            elif not instagram_link:  # If no links without 'www' have been found
+                instagram_link = href.replace('www.', '')
+        elif href and href.startswith('mailto') and not email_link:
+            if '@moxymgt.com' in href:
+                email_link = href.replace('mailto:', '')
+            elif not email_link:
+                email_link = href.replace('mailto:', '')
+    
+    # Extract the username from the meta tag
+    meta_tag = soup.find('meta', {'property': 'og:title'})
+    if meta_tag and meta_tag.get('content'):
+        username = meta_tag.get('content')
+    
+    return {'Username': username, 'Instagram Link': instagram_link, 'Email Link': email_link}
+
+# Read the URLs from a text file
+with open('urls.txt', 'r') as f:
+    urls = f.read().splitlines()
+
+# Initialize an empty list to store the results
+results = []
+
+# Create a multiprocessing Pool
+with Pool() as p:
+    # Scrape each URL and append the results to the list
+    for i, result in enumerate(p.imap(scrape_links, urls)):
+        results.append(result)
+        print(f"Progress: {i+1}/{len(urls)} ({(i+1)/len(urls)*100:.2f}%)")
+
+# Convert the list to a DataFrame
+df = pd.DataFrame(results)
+
+# Write the DataFrame to an Excel file
+df.to_excel('output.xlsx', index=False)
